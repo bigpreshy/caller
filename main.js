ecd8872fe96c1e6a404c8ac47b5c8dc6d7a52e47
Preshy <lukeprecious@gmail.com>
2020-11-09 07:05:27 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+
+    $('.num').click(function () {
+    	
+        var num = $(this);
+        var text = $.trim(num.find('.txt').clone().children().remove().end().text());
+        var telNumber = $('#telNumber');
+        $(telNumber).val(telNumber.val() + text);
+    });
+
+    $('.tel').attr('readonly');
+
+});
+
+function myFunction() {
+  var api = "https://wa.me/234";
+  var x = document.getElementById("test").elements[0].value; 
+  var what = api.concat(x);
+  window.location.href = what;
+  //document.getElementById("root").innerHTML = what;
+}
